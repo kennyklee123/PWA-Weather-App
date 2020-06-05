106b00bedc003eeed8b6770922a4832fe0aa8270
KennyL123 <55815393+kennyklee123@users.noreply.github.com>
2020-06-05 00:01:48 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react' ;
-import { fetchWeather } from './api.fetchWeather';
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 //value needs to be something in the state
 //onChange has to be something coming from the state 
 const App = () => {
     const[query,setQuery] = useState(''); 
+    const[weather, setWeather] = useState({});//{} means an empty object
     
     //an async function to search
     const search = async (e) => {
-        //if the pressed key = enter, we make a search
+        //handling the event where the pressed key is enter which makes a search
         if(e.key === 'Enter'){
-
+            const data = await fetchWeather(query)
+            console.log(data);
         }
     }
 
@@ -23,6 +25,7 @@ const App = () => {
                 placeholder = "Search ..."
                 value = {query} 
                 onChange = {(e) => setQuery(e.target.value)}
+                onKeyPress = {search}
             />
         </div>
     );
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -11,9 +11,8 @@ export const fetchWeather = async (query) => {
     const {data} = await axios.get(URL, { 
         params: {
             q: query, 
-            units: 'metric'
-            APPID: API_KEY,
-
+            units: 'metric',
+            APPID: API_KEY
         }
     });
     return data;
