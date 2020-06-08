9c50f0c0b0acfb441945ea555b640e75f2997df1
KennyL123 <55815393+kennyklee123@users.noreply.github.com>
2020-06-07 20:47:47 -0700

    Completed service worker and wrote the offline page

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -17,8 +17,8 @@ self.addEventListener('install', (event) => {
 }); 
 
 /*Listen for any requests, a promise is returned and for each request we return a fetch of that request
-  the fetch is another promise, so we handle a promise's .theb with another promise
-  we match all the request that the page is receiving like a request for the API call, to show an image
+  the fetch is another promise, so we handle a promise's .then with another promise
+  we match all the request that the page is receiving like a request for the API call, to show an image, etc
  .then: for all requests we fetch them again(the return part) because we don't want to store the data about the API, since they alwys want the current weather
  .catch it couldnt fetch the data which means no internet connection so we load offline.html
 */
@@ -34,5 +34,18 @@ self.addEventListener('fetch', () => {
 
 //Active the Service worker
 self.addEventListener('activate', () => {
-    
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);//we want to keep the cache we are working iwth
+    //caches.keys returns a promise that resolves to an array of Cache keys
+    //for the .then we receive cacheNames
+    // we loop through the cacheNames and  do the if check
+    //we end up with an arraylist of allowed caches since we used .map
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) //if the cache white list doesn't include the cache name, we delete it
+                    return caches.delete(cacheName);
+            })
+        ))
+    )
 }); 
